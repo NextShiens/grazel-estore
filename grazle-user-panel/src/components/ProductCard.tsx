cf0c33a04930f619a65319a16100c4c5186663cf
Muhammad Fida Hassan <148639888+Muhammad-Fida-Hassan@users.noreply.github.com>
2024-08-01 19:54:44 +0500

    feat: Update store logo and review card UI (#15)
    The code changes in the "detailProduct/[slug]/page.tsx" file update the logic for displaying the store logo and review card UI. The store logo is now fetched from the "singleProduct" object and displayed using the "Image" component. The review card UI is also updated to include the user's profile image and display the rating using the "Rating" component. These changes improve the visual presentation and user experience of the product detail page.
    
    Co-authored-by: Adilsagheer <adilsagheer051@gmail.com>

--- grazle-user-panel/src/components/ProductCard.tsx
+++ grazle-user-panel/src/components/ProductCard.tsx
@@ -1,14 +1,14 @@
 "use client";
-import React, { useState } from 'react';
-import Image from 'next/image';
-import { useRouter } from 'next/navigation';
-import { useDispatch } from 'react-redux';
-import { FaHeart, FaStar } from 'react-icons/fa';
-import { calculateFinalPrice } from '@/utils/priceCalculation';
-import { updateCart } from '@/features/features';
-import { toast } from 'react-toastify';
+import React, { useState } from "react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useDispatch } from "react-redux";
+import { FaHeart, FaStar } from "react-icons/fa";
+import { calculateFinalPrice } from "@/utils/priceCalculation";
+import { updateCart } from "@/features/features";
+import { toast } from "react-toastify";
 import Cart from "@/assets/CartVector.png";
-import { IconButton } from '@mui/material';
+import { IconButton } from "@mui/material";
 import {
   favoriteProductApi,
   addRecenetViewedApi,
@@ -23,14 +23,20 @@ interface ProductCardProps {
   offerId?: string;
 }
 
-const ProductCard: React.FC<ProductCardProps> = ({ product, width, offerId }) => {
+const ProductCard: React.FC<ProductCardProps> = ({
+  product,
+  width,
+  offerId,
+}) => {
   const router = useRouter();
   const dispatch = useDispatch();
   const [favoriteProducts, setFavoriteProducts] = useState<number[]>([]);
   const [isPending, setPending] = useState(false);
 
-
-  const {item, basePrice, price, discountInfo } = calculateFinalPrice(product, null);
+  const { item, basePrice, price, discountInfo } = calculateFinalPrice(
+    product,
+    null
+  );
 
   const goToDetail = () => {
     router.push(`/detailProduct/${product.id}`);
@@ -46,7 +52,7 @@ const ProductCard: React.FC<ProductCardProps> = ({ product, width, offerId }) =>
       discountInfo: discountInfo,
     };
     dispatch(updateCart({ type: null, product: updateProduct }));
-    toast.success('Item has been added to cart!');
+    toast.success("Item has been added to cart!");
   };
 
   async function onLiked(e: any, productId: any) {
@@ -76,29 +82,32 @@ const ProductCard: React.FC<ProductCardProps> = ({ product, width, offerId }) =>
     }
   }
 
-
   if (offerId && offerId !== product?.offer_id) return null;
 
   return (
-    <div 
-  onClick={goToDetail}
-  className="group lg:w-[98%] w-[95%] border mb-1 mt-2 lg:mt-[24px] rounded-2xl hover:border-[1px] border-[#b9b5b5] relative"
->
-  <div className="cursor-pointer">
-    <Image
-      alt="Product Image"
-      width={203}
-      height={203}
-      src={'/' + product.featured_image}
-      className="w-full h-[203px] object-cover rounded-2xl cursor-pointer"
-    />
+    <div
+      onClick={goToDetail}
+      className="group lg:w-[98%] w-[95%] border mb-1 mt-2 lg:mt-[24px] rounded-2xl hover:border-[1px] border-[#b9b5b5] relative"
+    >
+      <div className="cursor-pointer">
+        <Image
+          alt="Product Image"
+          width={203}
+          height={203}
+          src={product.featured_image}
+          className="w-full h-[203px] object-cover rounded-2xl cursor-pointer"
+          onError={(e: any) => {
+            console.error("Image failed to load:", e);
+            e.target.src = "/path/to/fallback-image.jpg";
+          }}
+        />
 
-    <div className="flex w-full justify-between items-center absolute px-[16px] top-[10px]">
-      <button className="md:text-[8px] text-[9px] rounded-3xl text-white bg-[#F70000] md:py-2 py-1 md:px-3">
+        <div className="flex w-full justify-between items-center absolute px-[16px] top-[10px]">
+          {/* <button className="md:text-[8px] text-[9px] rounded-3xl text-white bg-[#F70000] md:py-2 py-1 md:px-3">
         {discountInfo?.toUpperCase()}
-      </button>
+      </button> */}
 
-        {/* <IconButton
+          {/* <IconButton
           size="medium"
           onClick={(e) => onLiked(e, product.id)}
         >
@@ -108,54 +117,55 @@ const ProductCard: React.FC<ProductCardProps> = ({ product, width, offerId }) =>
             <Image src={heart} alt="like" />
           )}
         </IconButton> */}
-        <LikeButton productId={item?.id} />
+          <LikeButton productId={item?.id} />
+        </div>
+
+        <div className="p-3">
+          <p className="text-[16px] w-[80%] font-semibold line-clamp-2 flex mr-2">
+            {product.title}
+          </p>
+          <div className="flex items-center md:mt-[16px] mt-[8px] gap-1">
+            <span className="md:text-sm text-[9px] text-[#F69B26]">
+              {product.rating} ({product.reviews})
+            </span>
+            <FaStar size={12} color="#F69B26" />
+          </div>
+
+          <p className="md:text-[20px] text-[14px] text-[#FC3030] font-semibold md:mt-[16px] mt-[8px]">
+            ₹{typeof price === "number" ? price.toFixed(2) : price}
+          </p>
+
+          <div className="flex items-center md:mt-[16px] mt-[8px]">
+            <p className="md:text-[16px] text-[10px] text-[#909198] line-through font-normal">
+              ₹
+              {typeof basePrice === "number" ? basePrice.toFixed(2) : basePrice}
+            </p>
+
+            <p className="md:text-[16px] text-[10px] text-[#4FAD2E] ml-[24px] font-semibold">
+              {discountInfo}
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="hidden mb-3 flex justify-center opacity-0 group-hover:opacity-100 group-hover:flex w-full">
+        <button
+          className="text-[#F70000] w-[90%] h-[40px] border-[1px] border-[#F70001] rounded-lg"
+          onClick={(e) =>
+            onAddingCart(e, product, price, basePrice, discountInfo)
+          }
+        >
+          <div className="flex items-center justify-center">
+            <p className="font-semibold text-[14px]">Add to cart</p>
+            <Image
+              alt="cart"
+              src={Cart}
+              className="w-[20px] h-[20px] ml-[12px]"
+            />
+          </div>
+        </button>
+      </div>
     </div>
-
-    <div className="p-3">
-      <p className="text-[16px] w-[80%] font-semibold line-clamp-2 flex justify-center items-center mr-2">
-        {product.title}
-      </p>
-      <div className="flex items-center md:mt-[16px] mt-[8px] gap-1">
-        <span className="md:text-sm text-[9px] text-[#F69B26]">
-          {product.rating} ({product.reviews})
-        </span>
-        <FaStar size={12} color="#F69B26" />
-      </div>
-
-      <p className="md:text-[20px] text-[14px] text-[#FC3030] font-semibold md:mt-[16px] mt-[8px]">
-        ₹{typeof price === 'number' ? price.toFixed(2) : price}
-      </p>
-
-      <div className="flex items-center md:mt-[16px] mt-[8px]">
-        <p className="md:text-[16px] text-[10px] text-[#909198] line-through font-normal">
-          ₹{typeof basePrice === 'number' ? basePrice.toFixed(2) : basePrice}
-        </p>
-
-        <p className="md:text-[16px] text-[10px] text-[#4FAD2E] ml-[24px] font-semibold">
-          {discountInfo}
-        </p>
-      </div>
-    </div>
-  </div>
-
-  <div className="hidden mb-3 flex justify-center opacity-0 group-hover:opacity-100 group-hover:flex w-full">
-    <button
-      className="text-[#F70000] w-[90%] h-[40px] border-[1px] border-[#F70001] rounded-lg"
-      onClick={(e) =>
-        onAddingCart(e, product, price, basePrice, discountInfo)
-      }
-    >
-      <div className="flex items-center justify-center">
-        <p className="font-semibold text-[14px]">Add to cart</p>
-        <Image
-          alt="cart"
-          src={Cart}
-          className="w-[20px] h-[20px] ml-[12px]"
-        />
-      </div>
-    </button>
-  </div>
-</div>
   );
 };
 
