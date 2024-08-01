5c81ff03a6dd9d5498f3043c6b19cf72311246c4
umerfarok <umerfarooq.dev@gmail.com>
2024-08-01 20:46:18 +0500

    feat: Refactor ProtectedRoute component to use local storage helper functions
    The code changes in the `ProtectedRoute` component refactor the usage of `localStorage` to use helper functions `getLocalStorage` and `setLocalStorage`. This improves code readability and ensures consistent handling of local storage operations.

--- grazle-user-panel/src/components/ProtectedRoute.tsx
+++ grazle-user-panel/src/components/ProtectedRoute.tsx
@@ -2,12 +2,24 @@
 import { useAuth } from '@/app/AuthContext';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
+const getLocalStorage = (key) => {
+  if (typeof window !== 'undefined') {
+    return localStorage.getItem(key)
+  }
+  return null
+}
+
+const setLocalStorage = (key, value) => {
+  if (typeof window !== 'undefined') {
+    localStorage.setItem(key, value)
+  }
+}
 
 
 const ProtectedRoute = ({ children }) => {
   const { user, loading } = useAuth();
   const router = useRouter();
-  const token = localStorage.getItem("token")
+  const token = getLocalStorage("token")
 
   useEffect(() => {
     // if (!loading && !user) {
