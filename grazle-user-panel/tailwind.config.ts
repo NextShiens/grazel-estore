bb166127e7d1e34eb94fe8ec6abb5797f3a80826
umerfarok <umerfarooq.dev@gmail.com>
2024-08-04 18:35:55 +0500

    fix: Update image source URLs for product and offer pages

--- grazle-user-panel/tailwind.config.ts
+++ grazle-user-panel/tailwind.config.ts
@@ -13,6 +13,13 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      zIndex: {
+        '60': '60',
+        '70': '70',
+        '80': '80',
+        '90': '90',
+        '100': '100',
+      }
     },
   },
   plugins: [],
