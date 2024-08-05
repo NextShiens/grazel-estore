5592ef874d112ab54bc0e2ac8ce81809a081fd6b
umerfarok <umerfarooq.dev@gmail.com>
2024-08-05 11:08:04 +0500

    feat: Add new API endpoints for membership plans and user membership plans

--- SuperAdmin/src/components/sidebar.jsx
+++ SuperAdmin/src/components/sidebar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useRouter } from "next/navigation";
 import { useDispatch, useSelector } from "react-redux";
-import { updatePageLoader, updateSidebar } from "@/features/features";
+import { updatePageLoader, updateSidebar, updatePageNavigation } from "@/features/features";
 
 import { RxDashboard } from "react-icons/rx";
 import { LuShoppingBag } from "react-icons/lu";
@@ -22,24 +22,31 @@ import { PiRankingLight } from "react-icons/pi";
 import { IoMdPhotos } from "react-icons/io";
 import { BiCategory } from "react-icons/bi";
 import { CiLogout } from "react-icons/ci";
+import { MdCardMembership } from "react-icons/md"; // Added for Membership Plans icon
 import { login, logout } from "@/lib";
 import { toast } from "react-toastify";
 
 const Sidebar = () => {
-  const navigate = useRouter();
+  const router = useRouter();
   const dispatch = useDispatch();
   const showSidebar = useSelector((state) => state.showSidebar);
   const fn_sidebarControl = () => {
     dispatch(updateSidebar(!showSidebar));
   };
+
+  const handleNavigation = (path) => {
+    dispatch(updatePageLoader(true));
+    dispatch(updatePageNavigation(path));
+    router.push(`/${path}`);
+  };
+
   return (
     <>
       <div
-        className={`${
-          showSidebar
+        className={`${showSidebar
             ? "absolute md:relative flex h-[85%] min-h-[550px] md:h-auto"
             : "absolute md:relative hidden md:flex md:h-auto"
-        } w-[240px] bg-white rounded-tr-[8px] mt-[30px] shadow-2xl md:shadow-md px-[10px] py-[25px] flex-col gap-1.5 z-[999]`}
+          } w-[240px] bg-white rounded-tr-[8px] mt-[30px] shadow-2xl md:shadow-md px-[10px] py-[25px] flex-col gap-1.5 z-[999]`}
       >
         <button
           className="absolute md:hidden text-[var(--text-color)] right-5 scale-[1.5] top-2"
@@ -51,9 +58,9 @@ const Sidebar = () => {
           icon={<RxDashboard className="w-[20px] h-[20px]" />}
           label={"Dashboard"}
           navigateTo={"dashboard"}
-          navigate={navigate}
+          handleNavigation={handleNavigation}
         />
-        {/* <SidebarPageTemplate
+          {/* <SidebarPageTemplate
           icon={<LuShoppingBag className="w-[20px] h-[20px]" />}
           label={"Sales Overview"}
           navigateTo={"sales-overview"}
@@ -63,21 +70,21 @@ const Sidebar = () => {
           icon={<IoBagRemoveOutline className="w-[21px] h-[21px]" />}
           label={"Orders"}
           navigateTo={"orders"}
-          navigate={navigate}
+          handleNavigation={handleNavigation}
         />
         <SidebarPageTemplate
           icon={<FiShoppingBag className="w-[21px] h-[21px]" />}
           label={"Products"}
           navigateTo={"products"}
-          navigate={navigate}
+          handleNavigation={handleNavigation}
         />
         <SidebarPageTemplate
           icon={<BiCategory className="w-[21px] h-[21px]" />}
           label={"Categories"}
           navigateTo={"categories"}
-          navigate={navigate}
+          handleNavigation={handleNavigation}
         />
-        {/* <SidebarPageTemplate
+           {/* <SidebarPageTemplate
           icon={<RiDiscountPercentLine className="w-[20px] h-[20px]" />}
           label={"Offers"}
           navigateTo={"offers"}
@@ -87,63 +94,68 @@ const Sidebar = () => {
           icon={<FaUserTie className="w-[20px] h-[18px]" />}
           label={"Sellers"}
           navigateTo={"sellers"}
-          navigate={navigate}
+          handleNavigation={handleNavigation}
         />
         <SidebarPageTemplate
           icon={<BsPatchCheck className="w-[20px] h-[20px]" />}
           label={"Seller Verification"}
           navigateTo={"seller-verification"}
-          navigate={navigate}
+          handleNavigation={handleNavigation}
         />
         <SidebarPageTemplate
           icon={<HiOutlineCreditCard className="w-[21px] h-[21px]" />}
           label={"Credit Limit"}
           navigateTo={"credit-limit"}
-          navigate={navigate}
+          handleNavigation={handleNavigation}
         />
         <SidebarPageTemplate
           icon={<RiUserStarLine className="w-[20px] h-[20px]" />}
           label={"Customers"}
           navigateTo={"customers"}
-          navigate={navigate}
+          handleNavigation={handleNavigation}
         />
         <SidebarPageTemplate
           icon={<PiRankingLight className="w-[20px] h-[20px]" />}
           label={"Referral Ranking"}
           navigateTo={"referral-ranking"}
-          navigate={navigate}
+          handleNavigation={handleNavigation}
         />
         <SidebarPageTemplate
           icon={<IoMdPhotos className="w-[20px] h-[20px]" />}
           label={"Grazle Media"}
           navigateTo={"grazle-media"}
-          navigate={navigate}
+          handleNavigation={handleNavigation}
+        />
+        <SidebarPageTemplate
+          icon={<MdCardMembership className="w-[20px] h-[20px]" />}
+          label={"Membership Plans"}
+          navigateTo={"membership-plans"}
+          handleNavigation={handleNavigation}
         />
         <SidebarPageTemplate
           icon={<FiSettings className="w-[20px] h-[19px]" />}
-          label={` Settings`}
+          label={" Settings"}
           navigateTo={"settings"}
-          navigate={navigate}
+          handleNavigation={handleNavigation}
         />
         <SidebarPageTemplate
           icon={
             <HiOutlineExclamationCircle className="w-[20px] h-[20px] scale-[1.15]" />
           }
-          label={`Feedback`}
+          label={"Feedback"}
           navigateTo={"feedback"}
-          navigate={navigate}
+          handleNavigation={handleNavigation}
         />
         <SidebarPageTemplate
           icon={<CiLogout className="w-[20px] h-[19px]" />}
-          label={` Logout`}
+          label={" Logout"}
           navigateTo={null}
-          navigate={navigate}
+          handleNavigation={handleNavigation}
         />
       </div>
       <button
-        className={`${
-          showSidebar ? "hidden" : "absolute"
-        } md:hidden text-[var(--text-color)] left-5 scale-[1.5] top-[77px]`}
+        className={`${showSidebar ? "hidden" : "absolute"
+          } md:hidden text-[var(--text-color)] left-5 scale-[1.5] top-[77px]`}
         onClick={fn_sidebarControl}
       >
         <FaArrowAltCircleRight />
@@ -154,30 +166,36 @@ const Sidebar = () => {
 
 export default Sidebar;
 
-const SidebarPageTemplate = ({ icon, label, navigateTo, navigate }) => {
+const SidebarPageTemplate = ({ icon, label, navigateTo, handleNavigation }) => {
   const dispatch = useDispatch();
   const pageNavigation = useSelector((state) => state.pageNavigation);
+
+  const handleClick = async () => {
+    if (navigateTo === null) {
+      try {
+        await logout();
+        localStorage.clear();
+        sessionStorage.clear();
+        toast.success("Logout Successful");
+        setTimeout(() => {
+          window.location.reload();
+        }, 1000);
+      } catch (error) {
+        console.error("Logout failed:", error);
+        toast.error("Logout failed. Please try again.");
+      }
+    } else {
+      handleNavigation(navigateTo);
+    }
+  };
+
   return (
     <div
-      className={`flex h-[48px] items-center rounded-tr-full rounded-br-full gap-5 px-[17px] hover:text-[var(--text-color)] cursor-pointer hover:bg-[var(--bg-color)]  border-l-[2px] hover:border-[var(--text-color)] ${
-        pageNavigation === navigateTo
+      className={`flex h-[48px] items-center rounded-tr-full rounded-br-full gap-5 px-[17px] hover:text-[var(--text-color)] cursor-pointer hover:bg-[var(--bg-color)]  border-l-[2px] hover:border-[var(--text-color)] ${pageNavigation === navigateTo
           ? "text-[var(--text-color)] bg-[var(--bg-color)] border-[var(--text-color)]"
           : "text-gray-500 bg-transparent border-white"
-      }`}
-      onClick={async () => {
-        if (navigateTo == null) {
-          localStorage.clear();
-          sessionStorage.clear();
-          toast.success("Logout Sucessfully");
-          await logout();
-          setTimeout(() => {
-            window.location.reload();
-          }, 1000);
-        } else {
-          dispatch(updatePageLoader(true));
-          navigate.push(`/${navigateTo}`);
-        }
-      }}
+        }`}
+      onClick={handleClick}
     >
       {icon}
       <p className="text-[16px] font-[500]">{label}</p>
